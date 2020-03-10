f8b86b7b393f784789e29553137b998f8a7d600c
jonasblenk <jonas.blenk@beskgroup.com>
2020-03-10 16:36:06 +0100

    Change path of frontend page in docker to /chat/home

--- f-chat/src/main/java/de/thi/jbsa/prototype/view/ChatView.java
+++ f-chat/src/main/java/de/thi/jbsa/prototype/view/ChatView.java
@@ -13,7 +13,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route("chat")
+@Route("home")
 @Component
 public class ChatView
   extends VerticalLayout {
