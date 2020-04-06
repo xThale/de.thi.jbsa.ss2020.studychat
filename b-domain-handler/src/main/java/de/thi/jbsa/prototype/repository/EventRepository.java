e2cb433cd35087bcac7ef7c88f3e814ffa44015c
Balázs Endrédi <balazs.endredi@beskgroup.com>
2020-04-06 21:15:05 +0200

    Move entity and repo into right packages

--- b-domain-handler/src/main/java/de/thi/jbsa/prototype/repository/EventRepository.java
+++ b-domain-handler/src/main/java/de/thi/jbsa/prototype/repository/EventRepository.java
@@ -1,6 +1,7 @@
-package de.thi.jbsa.prototype;
+package de.thi.jbsa.prototype.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import de.thi.jbsa.prototype.model.EventEntity;
 
 /**
  * @author Christopher Timm <christopher.timm@beskgroup.com> on 27.02.18
